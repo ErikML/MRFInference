f0b2631b16cfcbf83a8a3a342de9ca2d5f1f537c
ErikML <lindgren@bu.edu>
2015-03-29 00:57:48 -0500

    added additional tests for independence checking

--- src/test/scala/test_mrf.scala
+++ src/test/scala/test_mrf.scala
@@ -20,6 +20,7 @@ class MrfSpec extends FlatSpec {
   it should "identify independence for all independence queries" in {
     assert(iUBMrf.checkIndependence(Set(1,2), Set(3,4), Set()) === true)
     assert(iUBMrf.checkIndependence(Set(1,3), Set(2), Set(4)) === true)
+    assert(iUBMrf.checkIndependence(Set(4), Set(3,2), Set(1)) === true)
   }
   
   behavior of "The 4-cycle, attractive, binary Mrf"
@@ -41,11 +42,39 @@ class MrfSpec extends FlatSpec {
   it should "identify dependencies for opposing corners without conditioning on the rest" in {
     assert(fCMrf.checkIndependence(Set(1), Set(3), Set()) === false)
     assert(fCMrf.checkIndependence(Set(2), Set(4), Set(3)) === false)
+    assert(fCMrf.checkIndependence(Set(4), Set(2), Set(3)) === false)
+    assert(fCMrf.checkIndependence(Set(3), Set(1), Set(2)) === false)
   }
   
-  it should "identify dependencies for pairs regardless of conditioning" in {
+  it should "identify dependencies for neighbors regardless of conditioning" in {
     assert(fCMrf.checkIndependence(Set(1), Set(2), Set(3,4)) === false)
     assert(fCMrf.checkIndependence(Set(2), Set(3), Set()) === false)
     assert(fCMrf.checkIndependence(Set(1), Set(4), Set(3)) === false)
+    assert(fCMrf.checkIndependence(Set(4), Set(3,2), Set(1)) === false)
+    assert(fCMrf.checkIndependence(Set(1,3), Set(2,3), Set()) === false)
+  }
+  
+  behavior of "The length-5-line, attractive, binary Mrf"
+  
+  val lFLFactor1 = Factor((1,2), attractiveFeature)
+  val lFLFactor2 = Factor((2,3), attractiveFeature)
+  val lFLFactor3 = Factor((3,4), attractiveFeature)
+  val lFLFactor4 = Factor((4,5), attractiveFeature)
+  val lFLFactors = Vector(lFLFactor1, lFLFactor2, lFLFactor3, lFLFactor4)
+  
+  val lFLMrf = new Mrf(lFLFactors, binaryDomain)
+  
+  it should "identify independencies when conditioned on a separating element" in {
+    assert(lFLMrf.checkIndependence(Set(4,5), Set(1,2), Set(3)) == true)
+    assert(lFLMrf.checkIndependence(Set(5), Set(2), Set(4)) == true)
+    assert(lFLMrf.checkIndependence(Set(1), Set(4), Set(2,3,5)) == true)
+    assert(lFLMrf.checkIndependence(Set(1,2,3), Set(5), Set(4)) == true)
+  }
+  
+  it should "identify dependencies without conditioning on a separating element" in {
+    assert(lFLMrf.checkIndependence(Set(2,3), Set(5), Set(1)) == false)
+    assert(lFLMrf.checkIndependence(Set(3,4,5), Set(2), Set()) == false)
+    assert(lFLMrf.checkIndependence(Set(2), Set(3), Set(1,5)) == false)
+    assert(lFLMrf.checkIndependence(Set(4), Set(5,1), Set(2,3)) == false)
   }
 }
